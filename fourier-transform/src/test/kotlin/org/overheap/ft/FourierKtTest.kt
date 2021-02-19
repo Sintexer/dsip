fc3caf346478ed9a0e15c3c1d8ad4a43a4e95ffd
Илья Буглаков <Neonlightnight@gmail.com>
2021-02-19 22:26:03 +0300

    migrate to: build.gradle

---
+++ fourier-transform/src/test/kotlin/org/overheap/ft/FourierKtTest.kt
@@ -0,0 +1,22 @@
+package org.overheap.ft
+
+import org.testng.Assert
+import org.testng.annotations.Test
+
+import org.testng.Assert.*
+
+class FourierKtTest {
+
+    @Test
+    fun testFormSequenceVector() {
+        assertTrue(true)
+    }
+
+    @Test
+    fun testGenerateMatrix() {
+    }
+
+    @Test
+    fun testCreateFrequencies() {
+    }
+}
