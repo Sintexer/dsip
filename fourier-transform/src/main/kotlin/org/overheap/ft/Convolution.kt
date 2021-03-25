2a1fbf48cff66ebfbaf3381df5e830fbac1a0e22
Darya Zalevskaya <50102121+Pokemon3108@users.noreply.github.com>
2021-03-25 20:50:40 +0300

    changed fft convolution and fft correlation

--- fourier-transform/src/main/kotlin/org/overheap/ft/Convolution.kt
+++ fourier-transform/src/main/kotlin/org/overheap/ft/Convolution.kt
@@ -1,22 +1,18 @@
 package org.overheap.ft
 
 import kscience.kmath.operations.Complex
-import kscience.kmath.operations.conjugate
 import kotlin.math.cos
 import kotlin.math.sin
 
 fun main() {
-    val amount = 4
-//    val vector = Convolution.createLinearConvolutionVector(amount) { x -> cos(2 * x) }
-//    val matrix = Convolution.createLinearConvolutionMatrix(amount) { x -> sin(5 * x) }
-//    val list = Convolution.createResult(matrix, vector)
+    val amount = 32
 
-    val v=Convolution.createCyclicConvolutionVector(amount){ x -> cos(2 * x) }
-    val m=Convolution.createCyclicConvolutionMatrix(amount){ x -> sin(5 * x) }
-    println(Convolution.createResult(m,v))
+    val v = Convolution.createCyclicConvolutionVector(amount) { x -> cos(2 * x) }
+    val m = Convolution.createCyclicConvolutionMatrix(amount) { x -> sin(5 * x) }
+    println(Convolution.createConvolutionResult(m, v))
 
-    val f1={x:Double -> cos(2 * x)}
-    println(Convolution.fftConvolution(amount, f1){ x -> sin(5 * x) })
+    val f1 = { x: Double -> cos(2 * x) }
+    println(Convolution.fftConvolution(amount, f1) { x -> sin(5 * x) })
 }
 
 object Convolution {
@@ -33,14 +29,14 @@ object Convolution {
 //        return (List(amount - 1) { 0.0 } + formValueSequence(amount, f))
 //    }
 
-    fun createResult(matrix: List<List<Double>>, vector: List<Double>): List<Double> {
+    fun createConvolutionResult(matrix: List<List<Double>>, vector: List<Double>): List<Double> {
         return multiply(matrix, vector)
-        //    .map { it / vector.size }
+            .map { it / vector.size }
     }
 
 
     fun createCyclicConvolutionVector(amount: Int, f: (Double) -> Double): List<Double> {
-        val vector= formValueSequence(amount, f).toDoubleArray()
+        val vector = formValueSequence(amount, f).toDoubleArray()
         vector.reverse(1, amount)
         return vector.toList()
     }
@@ -49,13 +45,13 @@ object Convolution {
         val orderedValues = formValueSequence(amount, f)
         val size = orderedValues.size
         return (0 until size).map { i ->
-            orderedValues.toList().subList(i, size)+orderedValues.toList().subList(0, i)
+            orderedValues.toList().subList(i, size) + orderedValues.toList().subList(0, i)
         }
     }
 
     fun fftConvolution(amount: Int, f1: (Double) -> Double, f2: (Double) -> Double): List<Double> {
-        val vector1= formValueSequence(amount, f1).map { Complex(it, 0) }
-        val vector2= formValueSequence(amount, f2).map { Complex(it, 0) }
+        val vector1 = FFT.fft(formValueSequence(amount, f1).map { Complex(it, 0) })
+        val vector2 = FFT.fft(formValueSequence(amount, f2).map { Complex(it, 0) })
         return fftOperation(vector1, vector2)
     }
 
--- fourier-transform/src/main/kotlin/org/overheap/ft/Correlation.kt
+++ fourier-transform/src/main/kotlin/org/overheap/ft/Correlation.kt
@@ -11,12 +11,12 @@ fun main() {
     val vector = Correlation.createCorrelationVector(amount) { x -> sin(5 * x) }
     println(Correlation.createCorrelationResult(matrix, vector))
 
-    val f1={x:Double -> cos(2 * x)}
-    println(Correlation.fftCorrelation(amount, f1){ x -> sin(5 * x) })
+    val f1 = { x: Double -> cos(2 * x) }
+    println(Correlation.fftCorrelation(amount, f1) { x -> sin(5 * x) })
 }
 
 object Correlation {
-//    fun createCorrelationMatrix(amount: Int, f: (Double) -> Double): List<List<Double>> {
+    //    fun createCorrelationMatrix(amount: Int, f: (Double) -> Double): List<List<Double>> {
 //        val orderedValues = formValueSequence(amount, f) + List(amount - 1) { 0.0 }
 //        val size = orderedValues.size
 //        return (0 until size).map { i ->
@@ -29,7 +29,7 @@ object Correlation {
 //    }
 //
     fun createCorrelationResult(matrix: List<List<Double>>, vector: List<Double>): List<Double> {
-        return Convolution.createResult(matrix, vector)
+        return Convolution.createConvolutionResult(matrix, vector)
     }
 
     fun createCorrelationVector(amount: Int, f: (Double) -> Double): List<Double> {
@@ -43,17 +43,19 @@ object Correlation {
             orderedValues.subList(size - i, size) + orderedValues.subList(0, size - i)
         }
     }
-    
+
     fun fftCorrelation(amount: Int, f1: (Double) -> Double, f2: (Double) -> Double): List<Double> {
-        val inverseVector1= formValueSequence(amount, f1).map { Complex(it, 0).conjugate }
-        val vector2= formValueSequence(amount,f2).map { Complex(it, 0) }
+        val inverseVector1 = FFT
+            .fft(formValueSequence(amount, f1).map { Complex(it, 0) })
+            .map { it.conjugate }
+        val vector2 =FFT.fft( formValueSequence(amount, f2).map { Complex(it, 0) })
         return fftOperation(inverseVector1, vector2)
     }
 }
 
-fun fftOperation(vector1: List<Complex>, vector2:List<Complex> ) : List<Double> {
-    val multiplicationList=vector1
+fun fftOperation(vector1: List<Complex>, vector2: List<Complex>): List<Double> {
+    val multiplicationList = vector1
         .zip(vector2)
-        .map { pair-> (pair.second*pair.first) }
+        .map { pair -> (pair.second * pair.first) }
     return FFT.ifft(multiplicationList)
 }
