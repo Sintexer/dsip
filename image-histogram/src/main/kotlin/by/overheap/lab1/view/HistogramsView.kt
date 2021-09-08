61bdf5d72286a334860302eb074c50aa4c376ba4
Ilya Buhlakou <neonlightnight@gmail.com>
2021-09-08 17:32:09 +0300

    image-histogram: add source file view butto

--- image-histogram/src/main/kotlin/by/overheap/lab1/view/HistogramsView.kt
+++ image-histogram/src/main/kotlin/by/overheap/lab1/view/HistogramsView.kt
@@ -2,6 +2,7 @@ package by.overheap.lab1.view
 
 import by.overheap.lab1.controller.ImageController
 import by.overheap.lab1.histogram.createHistogramColumns
+import javafx.beans.property.SimpleBooleanProperty
 import javafx.geometry.Pos
 import javafx.scene.chart.CategoryAxis
 import javafx.scene.chart.NumberAxis
@@ -29,12 +30,29 @@ class HistogramsView : View() {
     private val maxGreenHistogramData = mutableListOf<XYChart.Data<String, Number>>().asObservable()
     private val maxBlueHistogramData = mutableListOf<XYChart.Data<String, Number>>().asObservable()
 
+    private lateinit var sourceImageUrl: String
+    private var isDisabledViewImage = SimpleBooleanProperty(true)
+
     override val root = vbox(20, alignment = Pos.CENTER) {
         prefHeight = 900.0
         spacer { }
-        button("Choose file").action {
-            val file = chooseFile(filters = arrayOf()).first()
-            processImage(file)
+        hbox(30, alignment = Pos.CENTER) {
+            button("Choose file").action {
+                val file = chooseFile(filters = arrayOf()).first()
+                processImage(file)
+            }
+            button("Show source image") {
+                isDisable = true
+                disableWhen { isDisabledViewImage }
+            }.action {
+                find<ImageViewerFragment>(
+                    mapOf(
+                        ImageViewerFragment::imageUrl to sourceImageUrl,
+                        ImageViewerFragment::windowName to "Source image"
+                    )
+                )
+                    .openWindow(resizable = false)
+            }
         }
         barchart("Source", CategoryAxis(), NumberAxis()) {
             series("R", redHistogramData)
@@ -45,32 +63,32 @@ class HistogramsView : View() {
             animated = false
         }
         scrollpane {
-                vbox(alignment = Pos.CENTER) {
-                    barchart("Min filtered", CategoryAxis(), NumberAxis()) {
-                        series("R", minRedHistogramData)
-                        series("G", minGreenHistogramData)
-                        series("B", minBlueHistogramData)
-                        minHeight = 400.0
-                        minWidth = 1600.0
-                        animated = false
-                    }
-                    barchart("Min-max filtered", CategoryAxis(), NumberAxis()) {
-                        series("R", minMaxRedHistogramData)
-                        series("G", minMaxGreenHistogramData)
-                        series("B", minMaxBlueHistogramData)
-                        minHeight = 400.0
-                        minWidth = 1600.0
-                        animated = false
-                    }
-                    barchart("Max filtered", CategoryAxis(), NumberAxis()) {
-                        series("R", maxRedHistogramData)
-                        series("G", maxGreenHistogramData)
-                        series("B", maxBlueHistogramData)
-                        minHeight = 400.0
-                        minWidth = 1600.0
-                        animated = false
-                    }
+            vbox(alignment = Pos.CENTER) {
+                barchart("Min filtered", CategoryAxis(), NumberAxis()) {
+                    series("R", minRedHistogramData)
+                    series("G", minGreenHistogramData)
+                    series("B", minBlueHistogramData)
+                    minHeight = 400.0
+                    minWidth = 1600.0
+                    animated = false
                 }
+                barchart("Min-max filtered", CategoryAxis(), NumberAxis()) {
+                    series("R", minMaxRedHistogramData)
+                    series("G", minMaxGreenHistogramData)
+                    series("B", minMaxBlueHistogramData)
+                    minHeight = 400.0
+                    minWidth = 1600.0
+                    animated = false
+                }
+                barchart("Max filtered", CategoryAxis(), NumberAxis()) {
+                    series("R", maxRedHistogramData)
+                    series("G", maxGreenHistogramData)
+                    series("B", maxBlueHistogramData)
+                    minHeight = 400.0
+                    minWidth = 1600.0
+                    animated = false
+                }
+            }
 
         }
 
@@ -83,8 +101,32 @@ class HistogramsView : View() {
             greenHistogramData.setAll(createHistogramColumns(pixels.green).map { it.toChartData() })
             blueHistogramData.setAll(createHistogramColumns(pixels.blue).map { it.toChartData() })
         }
+        sourceImageUrl = file.toURI().toURL().toString()
+        isDisabledViewImage.set(false)
     }
 
+}
+
+class ImageViewerFragment : Fragment() {
+
+    val imageUrl: String by param()
+    val windowName: String by param()
+    private var windowHeight: Double = 500.0
+    private var windowWidth: Double = 500.0
+
+    override val root = scrollpane {
+        title = windowName
+        maxHeight = 900.0
+        maxWidth = 1800.0
+        vbox {
+            imageview(imageUrl, lazyload = false).also { i ->
+                windowHeight = i.image.height
+                windowWidth = i.image.width
+            }
+            prefHeight = windowHeight
+            prefWidth = windowWidth
+        }
+    }
 }
 
 private fun Map.Entry<Int, Int>.toChartData() = XYChart.Data<String, Number>(key.toString(), value)
